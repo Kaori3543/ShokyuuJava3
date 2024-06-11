4a5d38aab5deafaa44f281d15411cedbd899a16f
kaori23410157 <0cc42x022p.2v6j@au.com>
2024-06-11 09:51:05 +0900

    9-2キーボードに反応して動くようにする

--- 初級Java2.5/Car1.java
+++ 初級Java2.5/Car1.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car1 extends Vehicle{
+public class Car1 extends Vehicle implements KeyListener{
 	public Car1(int x,int y,int vx, int vy) {
 		super(x,y,vx,vy);
 	}
@@ -8,6 +10,25 @@ public class Car1 extends Vehicle{
 		frame.fillRect(x, y+20, 80, 20);
 		frame.fillOval(x+10, y+40, 20, 20);
 		frame.fillOval(x+50, y+40, 20, 20);
+	}
+	//キーから文字が入力されたとき
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	//キーが押されたとき
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("キーが押されました");
+		
+	}
+	//キーが離されたとき
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 
 
